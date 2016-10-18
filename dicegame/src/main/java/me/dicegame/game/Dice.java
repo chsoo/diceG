13e3962c665c38df44fd1f182b42c34569a643e3
woongbini <gamebook1004@gmail.com>
2016-10-18 18:07:59 +0900

    woongibin
    ddd

---
+++ dicegame/src/main/java/me/dicegame/game/Dice.java
@@ -0,0 +1,9 @@
+package me.dicegame.game;
+
+public class Dice {
+
+	public int roll() {
+		return (int)(Math.random()*6+1);
+	}
+
+}
--- dicegame/src/main/java/me/dicegame/hw/ScoreController.java
+++ dicegame/src/main/java/me/dicegame/hw/ScoreController.java
@@ -1,11 +1,20 @@
 package me.dicegame.hw;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import me.dicegame.game.Entry;
+
 @Controller
 public class ScoreController {
-
+	private ArrayList<Entry> scores;
+	
+	
+	
+	
+	
 	@RequestMapping("/score")
 	public String showConfigure() {
 		
